178326cf836800983617dfc2053cfed457ad30da
Patsaun7 <167927127+Patsaun7@users.noreply.github.com>
2025-03-19 09:54:56 -0400

    Add files via upload

---
+++ cinema-ebooking-system-main/cinema-backend######/src/main/java/cinema/Main.java
@@ -0,0 +1,18 @@
+package cinema;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"cinema", "cinema.config"})
+public class Main {
+
+    @Autowired
+    private DataInitializer dataInitializer;
+    
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
